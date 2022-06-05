088787710b2bb1e45448da42d9ff7d30e2d74d66
Boris Krasko <boris.krasko@live.com>
2022-06-05 19:41:35 +0200

    feat: done calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import crypto from 'crypto';
+const fd = fs.createReadStream('src/hash/files/fileToCalculateHashFor.txt');
+const hash = crypto.createHash('md5');
+hash.setEncoding('hex');
+
 export const calculateHash = async () => {
-    // Write your code here 
+  fd.on('end', function() {
+    hash.end();
+    console.log(hash.read());
+  });
+    
+  fd.pipe(hash);
 };
+
+calculateHash();
