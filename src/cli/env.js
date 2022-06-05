ee8d402687a577095dbe0a5d983a7d483788024d
Boris Krasko <boris.krasko@live.com>
2022-06-05 12:28:10 +0200

    refactor: tab size

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,6 @@
 const parseEnv = () => {
-    const processList = {...process.env}
-    console.log(processList);
+	const processList = {...process.env}
+	console.log(processList);
 };
 
 parseEnv();
